d0f8192012dec669dc0f372345b95210c4d9a186
Rodrigo Nunes <rodrigongalvan@hotmail.com>
2021-06-15 21:00:42 -0300

    Projeto Controle Remoto Java treinamento

---
+++ src/ControleRemoto.java
@@ -0,0 +1,14 @@
+
+class ControleRemoto {
+
+	public static void main(String[] args) {
+		  ControleRemoto1 c = new ControleRemoto1();
+		  c.ligar();
+		  c.maisVolume();
+		  c.play();
+		  c.abrirMenu();
+		  c.fecharMenu();
+		 
+	}
+
+}	 
